7f3a30e7991b443a38d405c882e4c81544678add
sanditya <sanditya2002@gmail.com>
2020-12-09 03:27:06 +0700

    Add Go Routines and Channels

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -21,14 +22,20 @@ var baseUrl string = "https://id.indeed.com/jobs?q=golang&limit=50"
 var jobUrl string = "https://id.indeed.com/lihat-lowongan-kerja?jk="
 
 func main() {
+	c := make(chan []jobsRes)
 	var jobs []jobsRes
 	pages := getPages()
 
 	for i := 0; i < pages; i++ {
-		jobsResult := getPage(i)
+		go getPage(i, c)
+	}
+	for i := 0; i < pages; i++ {
+		jobsResult := <-c
 		jobs = append(jobs, jobsResult...)
 	}
-	writeJobs(jobs)
+
+	writeJobs(jobs) //make csv file
+	fmt.Println(jobs)
 }
 
 func writeJobs(jobs []jobsRes) {
@@ -47,28 +54,38 @@ func writeJobs(jobs []jobsRes) {
 	}
 }
 
-func getPage(page int) []jobsRes {
+func getPage(page int, c chan<- []jobsRes) {
 	var jobs []jobsRes
+	d := make(chan jobsRes)
 	pageUrl := baseUrl + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting ", pageUrl)
 	res, err := http.Get(pageUrl)
+
 	checkErr(err)
 	checkStatus(res)
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
 
-	doc.Find(".jobsearch-SerpJobCard").Each(func(i int, s *goquery.Selection) {
-		job := extractJobs(s)
-		jobs = append(jobs, job)
+	searchedJob := doc.Find(".jobsearch-SerpJobCard")
+
+	searchedJob.Each(func(i int, s *goquery.Selection) {
+		go extractJobs(s, d)
 	})
-	return jobs
+
+	for i := 0; i < searchedJob.Length(); i++ {
+		job := <-d
+		jobs = append(jobs, job)
+	}
+	c <- jobs
 }
 
-func extractJobs(s *goquery.Selection) jobsRes {
+func extractJobs(s *goquery.Selection, d chan<- jobsRes) {
 	id, _ := s.Attr("data-jk")
 	id = cleanString(id)
 	title := cleanString(s.Find(".title>a").Text())
 	loc := cleanString(s.Find(".sjcl>.location").Text())
-	return jobsRes{id: id, title: title, loc: loc}
+	d <- jobsRes{id: id, title: title, loc: loc}
 }
 
 //get the total number of pages
